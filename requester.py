fb74f60ec74fbcb03c627b717a811b2b6f89e9f3
rvmoura <rvmoura.96@gmail.com>
2018-12-30 12:37:24 -0200

    Fixing some typos and implementing method for upcoming launches.

--- requester.py
+++ requester.py
@@ -3,8 +3,8 @@ from helpers import dict_creator
 
 
 class Requester():
-    def request_latest_lauch(self):
-        """Return the latest lauch info.
+    def request_latest_launch(self):
+        """Return the latest launch info.
 
         It should return the info about the latest launch as a json.
 
@@ -13,15 +13,23 @@ class Requester():
         return r.json()
 
     def request_next_launch(self):
-        """Return the next lauch info.
+        """Return the next launch info.
 
-        It should return the info about the next lauch as a json.
+        It should return the info about the next launch as a json.
         """
         r = requests.get('https://api.spacexdata.com/v3/launches/next')
+        return r.json()
+
+    def request_upcoming_launches(self):
+        """Return a list with upcoming launches."""
+        r = requests.get('https://api.spacexdata.com/v3/launches/upcoming')
         return r.json()
 
 
 if __name__ == '__main__':
     r = Requester()
-    print(f'Latest info\n{dict_creator(r.request_latest_lauch())}\n')
+    print(f'Latest info\n{dict_creator(r.request_latest_launch())}\n')
     print(f'Next lauch info\n{dict_creator(r.request_next_launch())}\n')
+
+    for launch in r.request_upcoming_launches():
+        print(f'{dict_creator(launch)}\n')
